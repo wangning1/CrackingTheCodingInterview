e88942c34e9a6f5b9b8eaf1333ec695b88851f9b
ningwang216580 <2631015638@qq.com>
2021-08-14 22:23:53 +0800

    旋转矩阵

---
+++ _01_07_RouteMatrix.java
@@ -0,0 +1,21 @@
+package com.winner;
+
+public class _01_07_RouteMatrix {
+
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int[][] newMatrix = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                newMatrix[j][n - i - 1] = matrix[i][j];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = newMatrix[i][j];
+            }
+        }
+    }
+
+}
