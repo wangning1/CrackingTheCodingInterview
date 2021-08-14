22ceaecd9d5ad3abba691a590e2702ce5e2029b3
ningwang216580 <2631015638@qq.com>
2021-08-14 22:26:04 +0800

    字符串轮转

---
+++ _01_09_FlipedString.java
@@ -0,0 +1,11 @@
+package com.winner;
+
+public class _01_09_FlipedString {
+    public boolean isFlipedString(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        return (s1 + s1).contains(s2);
+    }
+}
