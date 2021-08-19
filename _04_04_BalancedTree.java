0dcc28f6f3995dbe719b967af994e98e8dad4fb4
ningwang216580 <2631015638@qq.com>
2021-08-19 12:47:49 +0800

    平衡二叉树

---
+++ _04_04_BalancedTree.java
@@ -0,0 +1,21 @@
+public class _04_04_BalancedTree {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (Math.abs(depth(root.left) - depth(root.right)) > 1) {
+            return false;
+        }
+
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int depth(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return 1 + Math.max(depth(node.left), depth(node.right));
+    }
+
+
+}
